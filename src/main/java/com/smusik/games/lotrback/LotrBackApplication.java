be86f95ea52884002af17e5fbe092f9a5462e624
dgwiazda <damiangwiazdaa@wp.pl>
2021-02-24 20:07:51 +0100

    create branch feature-swagger

--- src/main/java/com/smusik/games/lotrback/LotrBackApplication.java
+++ src/main/java/com/smusik/games/lotrback/LotrBackApplication.java
@@ -2,6 +2,7 @@ package com.smusik.games.lotrback;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
 public class LotrBackApplication {
