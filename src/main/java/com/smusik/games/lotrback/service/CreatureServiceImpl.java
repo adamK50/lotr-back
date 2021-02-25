132cf37d4f837ad47e7a808e0cbb63ffc67e8283
dgwiazda <damiangwiazdaa@wp.pl>
2021-02-25 18:03:16 +0100

    poprawki w CreatureServiceImpl

--- src/main/java/com/smusik/games/lotrback/service/CreatureServiceImpl.java
+++ src/main/java/com/smusik/games/lotrback/service/CreatureServiceImpl.java
@@ -3,7 +3,6 @@ package com.smusik.games.lotrback.service;
 import com.smusik.games.lotrback.persistence.dao.CreatureRepository;
 import com.smusik.games.lotrback.persistence.model.CreatureEntity;
 import org.springframework.stereotype.Service;
-<<<<<<< HEAD
 import org.springframework.web.bind.annotation.RequestParam;
 
 @Service
@@ -15,16 +14,6 @@ public class CreatureServiceImpl implements CreatureService {
     @Override
     public CreatureEntity getCreatureEntityByName(@RequestParam String name) {
         return creatureService.getCreatureEntityByName(name);
-=======
 
-@Service
-public class CreatureServiceImpl {
-
-    private CreatureRepository creatureRepository;
-    private CreatureService creatureService;
-
-    public CreatureEntity getCreatureByName(String name) {
-        return creatureService.getCreatureByName(name);
->>>>>>> a99b67f... implementacja serwisu
     }
 }
