faca01d34d61a6f39ea2d16725b84bf47bbd968e
dgwiazda <75369265+dgwiazda@users.noreply.github.com>
2021-03-18 20:44:20 +0100

    Feature creatures tables (#9)
    * table creature

--- src/main/java/com/smusik/games/lotrback/persistence/model/FractionEntity.java
+++ src/main/java/com/smusik/games/lotrback/persistence/model/FractionEntity.java
@@ -5,6 +5,7 @@ import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import javax.persistence.*;
+import java.util.Objects;
 
 @Entity
 @Getter
@@ -16,8 +17,41 @@ public class FractionEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "FractionIdGenSeq")
     @SequenceGenerator(name = "FractionIdGenSeq", sequenceName = "FRACTION_ID_SEQ", allocationSize = 1)
+    @Column(name = "id")
     private Long id;
 
+    @Column(name = "fraction")
     private String fraction;
+    @Column(name = "destruction")
     private Integer destruction;
+
+    public FractionEntity(Long id, String fraction, Integer destruction) {
+        this.id = id;
+        this.fraction = fraction;
+        this.destruction = destruction;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        FractionEntity that = (FractionEntity) o;
+        return id.equals(that.id) &&
+                fraction.equals(that.fraction) &&
+                destruction.equals(that.destruction);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, fraction, destruction);
+    }
+
+    @Override
+    public String toString() {
+        return "FractionEntity{" +
+                "id=" + id +
+                ", fraction='" + fraction + '\'' +
+                ", destruction=" + destruction +
+                '}';
+    }
 }
